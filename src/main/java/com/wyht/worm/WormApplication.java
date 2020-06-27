da874e679c0a11e8b5ecd23a894cb9e3a2c8cb31
songwenbin <songwenbin@outlook.com>
2020-06-27 16:58:20 +0800

    springboot 项目框架搭建

---
+++ src/main/java/com/wyht/worm/WormApplication.java
@@ -0,0 +1,13 @@
+package com.wyht.worm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WormApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wyht/worm/WormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyht.worm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
